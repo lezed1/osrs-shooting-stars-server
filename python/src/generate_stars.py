f0d8deb8a2e5aa860108af079e3dbfc77c9ff7c2
Zander Bolgar <lezed1@gmail.com>
2023-10-15 13:23:17 -0400

    Restructure python code to separate out star logic

--- python/src/generate_stars.py
+++ python/src/generate_stars.py
@@ -4,27 +4,43 @@ import json
 import os
 import mysql.connector
 
-mydb = mysql.connector.connect(
+## Interesting code!
+
+
+def generate_star_info(connection):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT * FROM star_observation WHERE recorded_at > NOW() - INTERVAL 4 HOUR ORDER BY recorded_at DESC"
+        )
+
+        counts = defaultdict(int)
+
+        for row in cursor:
+            row = dict(zip(cursor.column_names, row))
+            counts[row["world"]] += 1
+
+    return {
+        "generated_at": datetime.datetime.now().isoformat(),
+        "counts": dict(counts),
+    }
+
+
+## run and save
+
+with mysql.connector.connect(
     host=os.environ["TYPEORM_HOST"],
     user=os.environ["TYPEORM_USERNAME"],
     password=os.environ["TYPEORM_PASSWORD"],
     database=os.environ["TYPEORM_DATABASE"],
     port=os.environ["TYPEORM_PORT"],
-)
+) as connection:
+    result = generate_star_info(connection)
 
-mycursor = mydb.cursor()
-
-mycursor.execute(
-    "SELECT * FROM star_observation WHERE recorded_at > NOW() - INTERVAL 4 HOUR ORDER BY recorded_at DESC"
-)
-
-counts = defaultdict(int)
-
-for row in mycursor:
-    row = dict(zip(mycursor.column_names, row))
-    counts[row["world"]] += 1
-
-result = {"generated_at": datetime.datetime.now().isoformat(), "counts": dict(counts)}
+temporary_filename = "out/new_json-pretty.json.json.json"
+final_filename = "out/stars-pretty.json"
+with open(temporary_filename, "w") as temp:
+    json.dump(result, temp, sort_keys=True, indent=2)
+os.rename(temporary_filename, final_filename)
 
 temporary_filename = "out/new_json.json.json.json"
 final_filename = "out/stars.json"
