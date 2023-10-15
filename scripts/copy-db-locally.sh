fa75c15137f168204b0364a5f001019841f406c6
Zander Bolgar <lezed1@gmail.com>
2023-10-15 12:52:56 -0400

    Add script to copy database locally!

---
+++ scripts/copy-db-locally.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ssh root@crowdsource.runescape.wiki -- \
+  "cd ~/osrs-shooting-stars-server/osrs-shooting-stars-server/hacky-prod-stuff \
+  && docker-compose exec -T db bash -c 'mysqldump -uroot -p\"\${MYSQL_ROOT_PASSWORD}\" shooting-stars --ignore-table=shooting-stars.star_observation'" \
+  | docker-compose exec -T db bash -c 'mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" shooting-stars'
+
+ssh root@crowdsource.runescape.wiki -- \
+  "cd ~/osrs-shooting-stars-server/osrs-shooting-stars-server/hacky-prod-stuff \
+  && docker-compose exec -T db bash -c 'mysqldump -uroot -p\"\${MYSQL_ROOT_PASSWORD}\" shooting-stars star_observation --where=\"recorded_at > NOW() - INTERVAL 8 HOUR\"'" \
+  | docker-compose exec -T db bash -c 'mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" shooting-stars'
